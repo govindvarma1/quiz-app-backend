04114162d8176ae16b2949399eaf95d80b5af5ee
Kallepalli Sai Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-10-12 14:15:26 +0530

    dbConnect function added to db.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
-import mongoose from "mongoose";
-import router from "./routes/routes.js";
 import dotenv from 'dotenv';
+import router from "./routes/routes.js";
+import DBConnect from "./config/db.js";
 
 const app = express();
 app.use(express.json());
@@ -9,15 +9,6 @@ dotenv.config();
 
 app.use("/", router);
 
-async function DBConnect(req, res) {
-    try {
-        await mongoose.connect(process.env.DB_URL);
-        console.log("Connected to database successfully");
-    } catch (err) {
-        console.error(`The following error occurred: ${err}`);
-        process.exit(1)
-    }
-}
 DBConnect();
 
 app.listen(process.env.PORT, (req, res) => {
